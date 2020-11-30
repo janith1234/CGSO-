dce151241affffbb33873077d8813eeb843b0903
janith1234 <74405014+janith1234@users.noreply.github.com>
2020-11-30 15:19:57 +0530

    bounceOff

--- sketch.js
+++ sketch.js
@@ -1,59 +1,39 @@
 var fixedRect, movingRect;
-var gameObject1, gameObject2, gameObject3, gameObject4;
 
 function setup() {
-  createCanvas(1200, 800);
-  fixedRect = createSprite(600, 400, 50, 80);
+  createCanvas(1200,800);
+  fixedRect = createSprite(400, 100, 50, 80);
   fixedRect.shapeColor = "green";
-
-  movingRect = createSprite(800, 400, 80, 30);
+  fixedRect.debug = true;
+  movingRect = createSprite(400, 800,80,30);
   movingRect.shapeColor = "green";
-
-  gameObject1 = createSprite(100, 100, 50, 50);
-  gameObject1.shapeColor = "green";
-  gameObject2 = createSprite(200, 100, 50, 50);
-  gameObject2.shapeColor = "green";
-  gameObject3 = createSprite(300, 100, 50, 50);
-  gameObject3.shapeColor = "green";
-  gameObject4 = createSprite(400, 100, 50, 50);
-  gameObject4.shapeColor = "green";
+  movingRect.debug = true;
+  Ironman=createSprite(400,400,20,20)
+  Ironman.shapeColor="red"
+  Ironman.debug=true;
+  Ironman.velocityY=2;
+  movingRect.velocityY = -5;
+  fixedRect.velocityY = +5;
 }
 
 function draw() {
-  background(0, 0, 0);
-  movingRect.x = World.mouseX;
-  movingRect.y = World.mouseY;
+  background(0,0,0);  
 
-  if (isTouching(movingRect, gameObject1)) {
-    movingRect.shapeColor = "blue";
-    gameObject1.shapeColor = "blue";
-  }
-  else {
-    movingRect.shapeColor = "green";
-    gameObject1.shapeColor = "green";
-  }
+  bounceOff(Ironman,movingRect);
 
-  if (isTouching(movingRect, gameObject2)) {
-    movingRect.shapeColor = "pink";
-    gameObject2.shapeColor = "pink";
-  }
-  else {
-    movingRect.shapeColor = "green";
-    gameObject2.shapeColor = "green";
-  }
-
+  bounceOff(movingRect,fixedRect);
   drawSprites();
 }
 
-function isTouching(object1, object2) {
-  if (object1.x - object2.x < object2.width / 2 + object1.width / 2
-    && object2.x - object1.x < object2.width / 2 + object1.width / 2
-    && object1.y - object2.y < object2.height / 2 + object1.height / 2
-    && object2.y - object2.y < object2.height / 2 + object1.height / 2) {
-
-    return true;
+function bounceOff(object1,object2){
+  if (object1.x - object2.x < object2.width/2 + object1.width/2
+    && object2.x - object1.x < object2.width/2 + object1.width/2) {
+    object1.velocityX = object1.velocityX * (-1);
+    object2.velocityX = object2.velocityX * (-1);
   }
-  else {
-    return false;
-  }
+  if (object1.y - object2.y < object2.height/2 + object1.height/2
+    && object2.y - object2.y < object2.height/2 + object1.height/2) {
+      object1.velocityY = object1.velocityY * (-1);
+      object2.velocityY = object2.velocityY * (-1);
+  } 
 }
